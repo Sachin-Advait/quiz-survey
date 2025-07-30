36402c61f3f24ae2bf3b29691567f68760901efa
Sachin Advait <sachinadvaita@gmail.com>
2025-07-30 18:29:04 +0530

    Fixed cors error.

--- src/main/java/com/gissoftware/quiz_survey/config/SecurityConfig.java
+++ src/main/java/com/gissoftware/quiz_survey/config/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOriginPatterns(List.of("*"));
+        config.setAllowedOrigins(List.of("http://localhost:5173", "https://quiz-survey.netlify.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
